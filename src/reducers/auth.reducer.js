d4b92681d90c93c47aae12cafb3c4425585018fc
Prajwal Shrestha <pjmessi25@gmail.com>
2021-01-10 22:47:18 +0545

    created auth reducer. changed context directory to contexts

---
+++ src/reducers/auth.reducer.js
@@ -0,0 +1,44 @@
+export const authReducer = (state, action) => {
+    
+    switch(action.type) {
+        case 'REQUEST_LOGIN':
+            return {
+                ...state,
+                loading: true
+            }
+
+        case 'LOGIN_SUCCESS':
+            return {
+                ...state,
+                loading: false,
+                user: action.payload.user,
+                token: action.payload.token
+            }
+
+        case 'LOGIN_ERROR':
+            return {
+                ...state,
+                loading: false,
+                error: action.error
+            }
+
+        case 'LOGOUT':
+            return {
+                ...state,
+                loading: false,
+                user: '',
+                token: ''
+            }
+
+        default: 
+            throw new Error(`Invalid action type: ${action.type}.`);
+    }
+
+}
+
+export const initialState = {
+    user: localStorage.getItem('user') || '',
+    token: localStorage.getItem('token') || '',
+    loading: false,
+    error: {}
+};
