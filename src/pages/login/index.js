7d302bd56e93052c34e72c9ff6401845b4569357
Prajwal Shrestha <pjmessi25@gmail.com>
2021-01-10 22:30:06 +0545

    created login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,25 @@
 const Login = () => {
-    return (
+    return <>
         <h1>Login page</h1>
-    )
+        <div>
+            <form action="">
+                <input 
+                    placeholder="email..."
+                    type="email" 
+                    name="email" 
+                    id="email"
+                />
+                <input 
+                    placeholder="password..."
+                    type="password" 
+                    name="password" 
+                    id="password"
+                />
+                <button type="submit">Login</button>
+            </form>
+
+        </div>
+    </>
 }
 
 export default Login;
