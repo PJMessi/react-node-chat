2a896c734709726ee26b5eaa21d081f4552254f8
Prajwal Shrestha <pjmessi25@gmail.com>
2021-01-12 21:42:40 +0545

    formatted time in chat list.

--- src/pages/dashboard/ChatList.js
+++ src/pages/dashboard/ChatList.js
@@ -1,22 +1,22 @@
 
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo } from "react";
 import { fetchMessages } from "../../actions/message.action";
 import { useMessageContext } from "../../contexts/messages.context";
 import { useAuthContext } from '../../contexts/auth.context';
+import moment from 'moment';
+
+const timefilter = (timestamp) => {
+  return moment(timestamp).format('MMMM Do YYYY, h:mm a');
+}
 
 const YourMessage = ({ message }) => {
   return (
     <>
       <div className="media media-chat media-chat-reverse">
-        <img
-          className="avatar"
-          src="https://img.icons8.com/color/36/000000/administrator-male.png"
-          alt="..."
-        />
         <div className="media-body">
           <p>{message.content}</p>
-          <p className="meta">
-            <time dateTime="2018">23:58</time>
+          <p className="meta" style={{color: 'lightgrey'}}>
+            <time dateTime="2018">{timefilter(message.createdAt)}</time>
           </p>
         </div>
       </div>
@@ -28,10 +28,15 @@ const TheirMessage = ({ message }) => {
   return (
     <>
       <div className="media media-chat">
+        <img
+          className="avatar"
+          src="https://img.icons8.com/color/36/000000/administrator-male.png"
+          alt="..."
+        />
         <div className="media-body">
           <p>{message.content}</p>
           <p className="meta">
-            <time dateTime="2018">00:06</time>
+            <time dateTime="2018">{timefilter(message.createdAt)}</time>
           </p>
         </div>
       </div>
@@ -47,15 +52,14 @@ const ChatList = () => {
   useEffect(() => {
     fetchMessages(messageDispatch).catch((error) => {
       console.log(error)
-    })
-  }, [])
+    });
+  }, []);
 
   const formattedMessages = useMemo(() => {
     const messages = messageState.messages;
     if (messages === '') return [];
     return [...messages].reverse();
-
-  }, [messageState.messages])
+  }, [messageState.messages]);
 
   return (
     <>
