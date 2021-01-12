d8af5b02388f6261d6d5b5ebdab21e4ccc81da1c
Prajwal Shrestha <pjmessi25@gmail.com>
2021-01-13 00:12:27 +0545

    added send message event.

--- src/pages/dashboard/MessageBox.js
+++ src/pages/dashboard/MessageBox.js
@@ -1,7 +1,18 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
-const MessageBox = () => {
+const MessageBox = ({ socket }) => {
+
+  const [message, setMessage] = useState('');
+
+  const emitMessage = async(e) => {
+    e.preventDefault();
+    if (message === '') return;
+    await socket.emit('chat-message', message)
+    setMessage('');
+  }
+
   return (
     <div className="publisher bt-1 border-light">
       <img
@@ -14,9 +25,11 @@ const MessageBox = () => {
         className="publisher-input"
         type="text"
         placeholder="Write something"
+        value={message}
+        onChange={(e) => {setMessage(e.target.value)}}
       />
 
-      <a className="publisher-btn text-info" href="#" data-abc="true">
+      <a className="publisher-btn text-info" href="#" data-abc="true" onClick={(e) => {emitMessage(e)}}>
         <FontAwesomeIcon icon={faPaperPlane} />
       </a>
       
